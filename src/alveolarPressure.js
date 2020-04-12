9ca4a914b0298a61fcbd83e447cab07417000cac
Victor Kmita <me@vkmita.com>
2020-04-12 15:53:07 +0700

    small tweaks

--- src/alveolarPressure.js
+++ src/alveolarPressure.js
@@ -1,11 +1,16 @@
-import { C02_PRESSURE, RESPIRATORY_QUOTIENT, WATER_VAPOR_PRESSURE } from './constants';
+import { 
+  C02_PRESSURE, 
+  RESPIRATORY_QUOTIENT, 
+  WATER_VAPOR_PRESSURE,
+} from './constants';
 
 // alveolar ventilation equation
 // calculates the partial pressure of the inert gas in the 
 // alvioli before it enters tissues
 const alveolarPressure = (ambiantPressure, gasRatio) => {
   // gas in the alveoli not coming from the gas mix
-  const alveolarGas = WATER_VAPOR_PRESSURE + ((1 - RESPIRATORY_QUOTIENT) * C02_PRESSURE)
+  const alveolarGas = 
+    WATER_VAPOR_PRESSURE + ((1 - RESPIRATORY_QUOTIENT) * C02_PRESSURE);
 
   return (ambiantPressure - alveolarGas) * gasRatio;
 }
