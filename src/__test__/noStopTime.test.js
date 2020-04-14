823705057a37d7c98de5304a652eb6f3ea1bd292
Victor Kmita <me@vkmita.com>
2020-04-14 11:20:28 +0700

    add test to 45

--- src/__test__/noStopTime.test.js
+++ src/__test__/noStopTime.test.js
@@ -1,7 +1,7 @@
 import noStopTime from '../noStopTime';
 import ZHL16B from '../ZH-L16B';
 
-test('noStopTime', () => {
+test('direct decent to 40', () => {
   // direct decent to 40 meters
   const depth = 40.0;
   const gasRatio = 0.79;
@@ -17,3 +17,20 @@ test('noStopTime', () => {
 
   expect(ndls).toEqual(10.489543795972834);
 })
+
+  test('direct decent to 45', () => {
+  // direct decent to 40 meters
+  const depth = 45.0;
+  const gasRatio = 0.79;
+  const compartment = ZHL16B[2].n2
+  const tissuePressure = 1.0670992600725278;
+
+  const ndls = noStopTime({
+    compartment,
+    depth,
+    gasRatio,
+    tissuePressure,
+  });
+
+  expect(ndls).toEqual(7.672695470139803);
+});
