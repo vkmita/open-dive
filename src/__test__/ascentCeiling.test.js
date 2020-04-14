37bcba10541e454cccc388dc83b6beafe07d65c9
Andrew Austin <andrew.d.ausitn2@gmail.com>
2020-04-14 13:49:05 +0700

    introduce ascent ceiling function and add test.

---
+++ src/__test__/ascentCeiling.test.js
@@ -0,0 +1,10 @@
+import ascentCeiling from '../ascentCeiling'
+import ZHL16B from '../ZH-L16B'
+
+test('ascentCeiling', () => {
+ const tissuePressure = 0.912248948856579
+ const compartment = ZHL16B[2]['n2']
+  console.log(compartment)
+  const ascentCeilingPressure = ascentCeiling (tissuePressure, compartment)
+  expect(ascentCeilingPressure).toEqual(-0.057161034714824414)
+})
---
+++ src/ascentCeiling.js
@@ -0,0 +1,8 @@
+
+const ascentCeiling = (tissuePressure, compartment) => {
+  const {a, b} = compartment 
+  const ascentPressureLimit = (tissuePressure - a) * b
+    return ascentPressureLimit
+};
+
+export default ascentCeiling
