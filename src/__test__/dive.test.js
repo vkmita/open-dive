de33c4229831716d7448932a67d5e0e38c18ef4d
Victor Kmita <me@vkmita.com>
2020-04-19 13:40:41 +0700

    add ndls test for when you pass them

--- src/__test__/dive.test.js
+++ src/__test__/dive.test.js
@@ -11,14 +11,16 @@ test('iniitializeDive', () => {
 });
 
 test('addSample', () => {
-  const { samples } = addSample({
+  const nDive = addSample({
     depth: 40,
     dive,
     time: dive.startedAt + 2 * 1000,
   });
 
+  let { samples } = dive;
+
   // most recent sammple
-  const sample = samples[samples.length - 1];
+  let sample = samples[samples.length - 1];
 
   expect(sample.tissues[4]).toEqual({
     he: 0,
@@ -29,5 +31,26 @@ test('addSample', () => {
     compartment: '2',
     gas: 'n2',
     value: 10.097413217365236,
+  });
+
+  // stay at 40 meters for 30 minutes
+  const mDive = addSample({
+    depth: 40,
+    dive: nDive,
+    time: dive.startedAt + 32 * 1000,
+  });
+
+  samples = mDive.samples;
+  sample = samples[samples.length - 1];
+
+  expect(sample.tissues[4]).toEqual({
+    he: 0,
+    n2: 2.90798318563608,
+  });
+
+  expect(sample.ndl).toEqual({
+    compartment: '2',
+    gas: 'n2',
+    value: -19.902586782634753,
   });
 });
